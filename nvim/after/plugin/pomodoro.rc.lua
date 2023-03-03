728f3f22c62be213a0b73beb198891363ee6665c
Robert I <117375850+robert-inez@users.noreply.github.com>
2023-03-03 14:08:58 -0700

    Delete pomodoro.rc.lua

--- nvim/after/plugin/pomodoro.rc.lua
+++
@@ -1,8 +0,0 @@
-local status, pomodoro = pcall(require, "pomodoro")
-if not status then
-	return
-end
-
-pomodoro.setup()
-vim.keymap.set("n", "<leader>pt", "<Cmd>PomodoroStart<CR>")
-vim.keymap.set("n", "<leader>pe", "<Cmd>PomodoroStop<CR>")
